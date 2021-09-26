218b920ee61a127a11e7da2aa9688f3e2da8c9a6
GitHub Action <action@github.com>
2021-09-26 16:37:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/willabides/intellijhelloplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.willabides.intellijhelloplugin.services
+
+import com.github.willabides.intellijhelloplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
